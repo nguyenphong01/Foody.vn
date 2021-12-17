dc1f40816eb64307fe62bcd0f71ccd1374450595
49_1951060914_NguyenHongPhong <nguyenphong15022001@gmail.com>
2021-12-17 22:26:53 +0700

    upload file

---
+++ giamgia.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
+</head>
+<body>
+    <div class="new-feature-header ">
+        <div class="new-feature-header__user-line">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12 alr">
+                        <div class="heading-news">
+                            <a href="https://jamja.vn/tuyen-dung/" rel="nofollow" target="_blank">
+                                <span class="jicon_users_o">                 
+                                </span><!-- react-text: 11 -->JAMJA đang tuyển dụng - Nhiều vị trí! Ứng tuyển ngay<!-- /react-text -->
+                                <span class="jicon_chevron_right_o">
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
+</body>
+</html>
