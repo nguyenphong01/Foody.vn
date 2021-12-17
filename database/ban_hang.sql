1ccbc5b7d2cc3ff193385df42ce7b9ae4ffd9422
Dưỡng Nguyễn Bá <duong5847@gmail.com>
2021-12-17 21:14:44 +0700

    Upload file

---
+++ database/ban_hang.sql
@@ -0,0 +1,283 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th12 17, 2021 lúc 03:12 PM
+-- Phiên bản máy phục vụ: 10.4.21-MariaDB
+-- Phiên bản PHP: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `ban_hang`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `chitiethoadon`
+--
+
+CREATE TABLE `chitiethoadon` (
+  `id_chi_tiet_hoadon` bigint(10) NOT NULL,
+  `sodh` bigint(20) NOT NULL,
+  `masp` bigint(20) NOT NULL,
+  `soluong` int(11) NOT NULL,
+  `dongia` float NOT NULL,
+  `thanhtien` decimal(9,2) NOT NULL,
+  `madv` varchar(200) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `chitiethoadon`
+--
+
+INSERT INTO `chitiethoadon` (`id_chi_tiet_hoadon`, `sodh`, `masp`, `soluong`, `dongia`, `thanhtien`, `madv`) VALUES
+(144, 127, 214, 1, 214, '214.00', '15'),
+(145, 128, 213, 1, 320, '320.00', ''),
+(146, 129, 214, 1, 100, '100.00', '');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `dichvu`
+--
+
+CREATE TABLE `dichvu` (
+  `madv` bigint(20) NOT NULL,
+  `tendv` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `gia` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `dichvu`
+--
+
+INSERT INTO `dichvu` (`madv`, `tendv`, `gia`) VALUES
+(15, 'Hâm nóng', 10),
+(16, 'Để nguội', 20);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `hoadon`
+--
+
+CREATE TABLE `hoadon` (
+  `sodh` bigint(20) NOT NULL,
+  `emailkh` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `ngaygiao` date NOT NULL,
+  `tenkh` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `diachi` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  `dienthoai` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `hinhthucthanhtoan` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `thanhtien` decimal(9,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `hoadon`
+--
+
+INSERT INTO `hoadon` (`sodh`, `emailkh`, `ngaygiao`, `tenkh`, `diachi`, `dienthoai`, `hinhthucthanhtoan`, `thanhtien`) VALUES
+(127, 'nghongphong@gmail.com', '2021-06-26', 'Phong', 'Hà Nội', '1234567890', 'ATM', '224.00'),
+(128, 'ngabduong0611@gmail.com', '2021-10-29', 'Duong', 'Ha Nội', '0852635623', 'ATM', '0.00'),
+(129, 'ngdangtruong@gmail.com', '2021-10-29', 'Truong', 'Bắc Ninh', '0852635623', 'ATM', '0.00');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `loginadmin`
+--
+
+CREATE TABLE `loginadmin` (
+  `tendangnhap` varchar(100) NOT NULL,
+  `matkhau` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `loginadmin`
+--
+
+INSERT INTO `loginadmin` (`tendangnhap`, `matkhau`) VALUES
+('admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `loginuser`
+--
+
+CREATE TABLE `loginuser` (
+  `email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `matkhau` int(200) NOT NULL,
+  `HoTen` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `DienThoai` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `loginuser`
+--
+
+INSERT INTO `loginuser` (`email`, `matkhau`, `HoTen`, `DienThoai`) VALUES
+('abc@gmail.com', 123, 'abc', '136888999'),
+('kimanh@gmail.com', 123, 'Kim Anh', '0934237266'),
+('ngbaduong@gmail.com', 123, 'Duong', '0852635623'),
+('ngdangtruong@gmail.com', 123, 'Truong', '122335848'),
+('nghongphong@gmail.com', 123, 'Hong PHong', '0833454666');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `nhacungcap`
+--
+
+CREATE TABLE `nhacungcap` (
+  `ID` bigint(10) NOT NULL,
+  `Ten` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `nhacungcap`
+--
+
+INSERT INTO `nhacungcap` (`ID`, `Ten`) VALUES
+(15, 'Bếp nhà An'),
+(16, 'Bếp nhà Chiến'),
+(17, 'Bếp nhà Phong'),
+(18, 'Bếp nhà Trường');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `sanpham`
+--
+
+CREATE TABLE `sanpham` (
+  `ID` bigint(10) NOT NULL,
+  `Ten` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `Gia` double NOT NULL,
+  `HinhAnh` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `Manhacungcap` bigint(10) NOT NULL,
+  `Mota` text COLLATE utf8_unicode_ci NOT NULL,
+  `KhuyenMai` tinyint(1) NOT NULL,
+  `makhuyenmai` varchar(10) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `chitiethoadon`
+--
+ALTER TABLE `chitiethoadon`
+  ADD PRIMARY KEY (`id_chi_tiet_hoadon`),
+  ADD KEY `chitiethoadon` (`sodh`);
+
+--
+-- Chỉ mục cho bảng `dichvu`
+--
+ALTER TABLE `dichvu`
+  ADD PRIMARY KEY (`madv`);
+
+--
+-- Chỉ mục cho bảng `hoadon`
+--
+ALTER TABLE `hoadon`
+  ADD PRIMARY KEY (`sodh`);
+
+--
+-- Chỉ mục cho bảng `loginadmin`
+--
+ALTER TABLE `loginadmin`
+  ADD PRIMARY KEY (`tendangnhap`);
+
+--
+-- Chỉ mục cho bảng `loginuser`
+--
+ALTER TABLE `loginuser`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Chỉ mục cho bảng `nhacungcap`
+--
+ALTER TABLE `nhacungcap`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Chỉ mục cho bảng `sanpham`
+--
+ALTER TABLE `sanpham`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `Ten` (`Ten`),
+  ADD KEY `sanpham` (`Manhacungcap`),
+  ADD KEY `makhuyenmai_1` (`makhuyenmai`),
+  ADD KEY `makhuyenmai_2` (`makhuyenmai`),
+  ADD KEY `makhuyenmai_3` (`makhuyenmai`),
+  ADD KEY `makhuyenmai_4` (`makhuyenmai`),
+  ADD KEY `makhuyenmai_5` (`makhuyenmai`);
+ALTER TABLE `sanpham` ADD FULLTEXT KEY `Ten_2` (`Ten`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `chitiethoadon`
+--
+ALTER TABLE `chitiethoadon`
+  MODIFY `id_chi_tiet_hoadon` bigint(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=147;
+
+--
+-- AUTO_INCREMENT cho bảng `dichvu`
+--
+ALTER TABLE `dichvu`
+  MODIFY `madv` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT cho bảng `hoadon`
+--
+ALTER TABLE `hoadon`
+  MODIFY `sodh` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=130;
+
+--
+-- AUTO_INCREMENT cho bảng `nhacungcap`
+--
+ALTER TABLE `nhacungcap`
+  MODIFY `ID` bigint(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT cho bảng `sanpham`
+--
+ALTER TABLE `sanpham`
+  MODIFY `ID` bigint(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=437;
+
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `chitiethoadon`
+--
+ALTER TABLE `chitiethoadon`
+  ADD CONSTRAINT `chitiethoadon` FOREIGN KEY (`sodh`) REFERENCES `hoadon` (`sodh`) ON DELETE CASCADE;
+
+--
+-- Các ràng buộc cho bảng `sanpham`
+--
+ALTER TABLE `sanpham`
+  ADD CONSTRAINT `sanpham` FOREIGN KEY (`Manhacungcap`) REFERENCES `nhacungcap` (`ID`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
