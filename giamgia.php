2918e9d88512c7747947c8064e03270c4acda20e
49_1951060914_NguyenHongPhong <nguyenphong15022001@gmail.com>
2021-12-18 08:59:45 +0700

    Update giamgia.php

--- giamgia.php
+++ giamgia.php
@@ -29,6 +29,38 @@
         </div>
     </div>
 
+    <div class="container new-feature-search-bar">
+      <div class="logo">
+        <a title="Thông tin khuyến mãi, giảm giá, sale, ưu đãi, coupon, voucher... về ăn uống, thời trang, mỹ phẩm, làm đẹp năm 2019 trên toàn quốc - JAMJA" class="" href="/"><img src="/assets/images/jamja-logo-2018-2x.png" alt="" height="56">
+        </a></div><div class="search-box">
+          <span class="jicon_search"></span>
+          <span class="search-box-label mr-2 ml-1">Tìm</span>
+          <div class="results-box " style="width: 428px;">
+            <input type="text" class="search-input" autocomplete="off" placeholder="khuyến mãi, thương hiệu, sản phẩm..." id="inputSearch" value="">
+            <div class="suggest-box">
+              <div class="suggest-wrapper">
+                <div class="suggest-word">
+                  <a href="javascript:;"></a>
+                </div>
+                <div class="suggest-word ">
+                  <a href="javascript:;"><!-- react-text: 28 --> <!-- /react-text --><!-- react-text: 29 -->Silky Beauty<!-- /react-text --><!-- react-text: 30 -->  <!-- /react-text --></a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <span class="jicon_map_pin_o"></span>
+          <span class="search-box-label mr-2">Ở</span>
+          <select name="location" id="home-page-choose-location" class="form-control choose-location">
+            <option class="location" value="ha-noi">Hà Nội</option>
+            <option class="location" value="tp-hcm">Tp. Hồ Chí Minh</option>
+            <option class="location" value="da-nang">Đà Nẵng</option>
+            <option class="location" value="hai-phong">Hải Phòng</option>
+          </select>
+        </div>
+      </div><!-- react-empty: 38 -->
+    </div>
+
+    
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
