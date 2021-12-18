ed3066b971bfa6a4e098d9bb537e6c794294bc72
49_1951060914_NguyenHongPhong <nguyenphong15022001@gmail.com>
2021-12-18 10:28:34 +0700

    upload file

--- giamgia.php
+++ giamgia.php
@@ -5,60 +5,63 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="css/giamgia.css">
 </head>
 <body>
-    <div class="new-feature-header ">
-        <div class="new-feature-header__user-line">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-12 alr">
-                        <div class="heading-news">
-                            <a href="https://jamja.vn/tuyen-dung/" rel="nofollow" target="_blank">
-                                <span class="jicon_users_o">                 
-                                </span><!-- react-text: 11 -->JAMJA đang tuyển dụng - Nhiều vị trí! Ứng tuyển ngay<!-- /react-text -->
-                                <span class="jicon_chevron_right_o">
-                                </span>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+  <div class="new-feature-header ">
+    <div class="new-feature-header__user-line">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 alr">
+            <div class="heading-news">
+              <a href="https://jamja.vn/tuyen-dung/" rel="nofollow" target="_blank">
+                <span class="jicon_users_o">                 
+                </span><!-- react-text: 11 -->JAMJA đang tuyển dụng - Nhiều vị trí! Ứng tuyển ngay<!-- /react-text -->
+                <span class="jicon_chevron_right_o"></span>
+              </a>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-
+  
     <div class="container new-feature-search-bar">
       <div class="logo">
         <a title="Thông tin khuyến mãi, giảm giá, sale, ưu đãi, coupon, voucher... về ăn uống, thời trang, mỹ phẩm, làm đẹp năm 2019 trên toàn quốc - JAMJA" class="" href="/"><img src="/assets/images/jamja-logo-2018-2x.png" alt="" height="56">
-        </a></div><div class="search-box">
-          <span class="jicon_search"></span>
-          <span class="search-box-label mr-2 ml-1">Tìm</span>
-          <div class="results-box " style="width: 428px;">
-            <input type="text" class="search-input" autocomplete="off" placeholder="khuyến mãi, thương hiệu, sản phẩm..." id="inputSearch" value="">
-            <div class="suggest-box">
-              <div class="suggest-wrapper">
-                <div class="suggest-word">
-                  <a href="javascript:;"></a>
-                </div>
-                <div class="suggest-word ">
-                  <a href="javascript:;"><!-- react-text: 28 --> <!-- /react-text --><!-- react-text: 29 -->Silky Beauty<!-- /react-text --><!-- react-text: 30 -->  <!-- /react-text --></a>
-                </div>
+        </a>
+      </div>
+      <div class="search-box">
+        <span class="material-icons-outlined">search</span>
+        <span class="search-box-label mr-2 ml-1">Tìm</span>
+        <div class="results-box " style="width: 428px;">
+          <input type="text" class="search-input" autocomplete="off" placeholder="khuyến mãi, thương hiệu, sản phẩm..." id="inputSearch" value="">
+          <div class="suggest-box">
+            <div class="suggest-wrapper">
+              <div class="suggest-word">
+                <a href="javascript:;"></a>
+              </div>
+              <div class="suggest-word ">
+                <a href="javascript:;"><!-- react-text: 28 --> <!-- /react-text --><!-- react-text: 29 -->Silky Beauty<!-- /react-text --><!-- react-text: 30 -->  <!-- /react-text --></a>
               </div>
             </div>
           </div>
-          <span class="jicon_map_pin_o"></span>
-          <span class="search-box-label mr-2">Ở</span>
-          <select name="location" id="home-page-choose-location" class="form-control choose-location">
-            <option class="location" value="ha-noi">Hà Nội</option>
-            <option class="location" value="tp-hcm">Tp. Hồ Chí Minh</option>
-            <option class="location" value="da-nang">Đà Nẵng</option>
-            <option class="location" value="hai-phong">Hải Phòng</option>
-          </select>
         </div>
-      </div><!-- react-empty: 38 -->
-    </div>
+        <span class="material-icons-outlined">place</span>
+        <span class="search-box-label mr-2">Ở</span>
+        <select name="location" id="home-page-choose-location" class="form-control choose-location">
+          <option class="location" value="ha-noi">Hà Nội</option>
+          <option class="location" value="tp-hcm">Tp. Hồ Chí Minh</option>
+          <option class="location" value="da-nang">Đà Nẵng</option>
+          <option class="location" value="hai-phong">Hải Phòng</option>
+        </select>
+      </div>
+    </div><!-- react-empty: 38 -->
+  </div>
 
     
 
