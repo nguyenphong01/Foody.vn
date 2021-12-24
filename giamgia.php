f3543f5b619e99cf95a6414ecbd80b277d270ba2
49_1951060914_NguyenHongPhong <nguyenphong15022001@gmail.com>
2021-12-24 17:59:35 +0700

    upload file

--- giamgia.php
+++ giamgia.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Thông tin khuyến mãi, giảm giá, sale, ưu đãi, coupon, voucher... về ăn uống, thời trang, mỹ phẩm, làm đẹp năm 2019 trên toàn quốc</title>
+    <title>Thông tin khuyến mãi, giảm giá, sale, ưu đãi, coupon, voucher... về ăn uống, thời trang, mỹ phẩm, làm đẹp năm
+        2019 trên toàn quốc</title>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -12,32 +14,37 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="css/giamgia.css">
 </head>
+
 <body>
 
-<header id="header">
-    <div class="container-topbar">
-      <a href="https://jamja.vn/tuyen-dung/" rel="nofollow" target="_blank">
-        <div class="text">
-          <span class="material-icons-outlined" >people_alt</span>JAMJA đang tuyển dụng - Nhiều vị trí! Ứng tuyển ngay >
+    <header id="header">
+        <div class="container-topbar">
+            <a href="https://jamja.vn/tuyen-dung/" rel="nofollow" target="_blank">
+                <div class="text">
+                    <span class="material-icons-outlined">people_alt</span>JAMJA đang tuyển dụng - Nhiều vị trí! Ứng
+                    tuyển ngay >
+                </div>
+                <span class="jicon_chevron_right_o"></span>
+            </a>
         </div>
-        <span class="jicon_chevron_right_o"></span>
-      </a>
-    </div>
-    <div class="container-bottom">
-      <div class="logo">
-          <a title="Thông tin khuyến mãi, giảm giá, sale, ưu đãi, coupon, voucher... về ăn uống, thời trang, mỹ phẩm, làm đẹp năm 2019 trên toàn quốc - JAMJA" class="" href="/">
-            <img src="img/jamja-logo.png" alt="" height="56">
-          </a>
-      </div>
+        <div class="container-bottom">
+            <div class="logo">
+                <a title="Thông tin khuyến mãi, giảm giá, sale, ưu đãi, coupon, voucher... về ăn uống, thời trang, mỹ phẩm, làm đẹp năm 2019 trên toàn quốc - JAMJA"
+                    class="" href="/">
+                    <img src="img/jamja-logo.png" alt="" height="56">
+                </a>
+            </div>
 
-      <div class="search-box ">
-        <div class="a">
-          <span class="material-icons-outlined">search</span>
-          <span class="search-box-label mr-2 ml-1">Tìm</span>
-        </div>
-        <div class="results-box " >
-          <input type="text" class="search-input" autocomplete="off" placeholder="   khuyến mãi, thương hiệu, sản phẩm..." id="inputSearch" value="" style="width: 428px; height:50px" >
-          <!--<div class="suggest-box">
+            <div class="search-box ">
+                <div class="a">
+                    <span class="material-icons-outlined">search</span>
+                    <span class="search-box-label mr-2 ml-1">Tìm</span>
+                </div>
+                <div class="results-box ">
+                    <input type="text" class="search-input" autocomplete="off"
+                        placeholder="   khuyến mãi, thương hiệu, sản phẩm..." id="inputSearch" value=""
+                        style="width: 428px; height:50px">
+                    <!--<div class="suggest-box">
             <div class="suggest-wrapper">
               <div class="suggest-word">
                 <a href="javascript:;">Toco Toco</a>
@@ -47,160 +54,91 @@
               </div>
             </div>
           </div>-->
-        </div>
-      </div>
-      <div class="select">
-        <div class="b">
-          <span class="material-icons-outlined">place</span>
-          <span class="search-box-label mr-2">Ở</span>
-        </div>
-        <select class="form-select" aria-label="Default select example" style="width:200px; height:50px">
-          <option selected>Hà Nội</option>
-          <option value="1">Tp.Hồ Chí Minh</option>
-          <option value="2">Đà Nẵng</option>
-          <option value="3">Hải Phòng</option>
-        </select>
-      </div>
-    </div>
-
-
-</header>
-
-<div id="container">
-    <div class="home_page">
-      <div class="home_page_left">
-        <div class="category_list_menu">
-            <div class="menu_first">
-                <a class="category-menu-item" href="/">
-                    <div class="category-menu-item_left">
-                      <span class="material-icons-outlined">assignment_turned_in</span>
-                    </div>
-                    <span >Đặt chỗ khuyến mãi</span>
-                    <div class="category-menu-item_right">
-                        <span class="material-icons-outlined">chevron_right</span>
-                    </div>
-                </a>
+                </div>
+            </div>
+            <div class="select">
+                <div class="b">
+                    <span class="material-icons-outlined">place</span>
+                    <span class="search-box-label mr-2">Ở</span>
+                </div>
+                <select class="form-select" aria-label="Default select example" style="width:200px; height:50px">
+                    <option selected>Hà Nội</option>
+                    <option value="1">Tp.Hồ Chí Minh</option>
+                    <option value="2">Đà Nẵng</option>
+                    <option value="3">Hải Phòng</option>
+                </select>
             </div>
         </div>
+    </header>
 
-        <ul class="home-page-category-list">
-          <li class="category-menu-item category-menu-item--header">
-            <a href="javascript:;" class="category-menu-item__url">
-              <span>Danh mục</span>
-            </a>
-          </li>
-          <li class="category-menu-item">
-            <a class="category-menu-item__url" href="/an">
-              <div class="category-menu-item__url__left">
-                <span class="material-icons-outlined">restaurant</span>
-              </div>
-              <span>Đồ ăn</span>
-              <div class="category-menu-item_right">
-                <span class="material-icons-outlined">chevron_right</span>
-              </div>
-            </a>
-          </li>
-          <li class="category-menu-item">
-            <a class="category-menu-item__url" href="/uong">
-              <div class="category-menu-item__url__left">
-                <span class="material-icons-outlined">local_cafe</span>
-              </div>
-              <span>Đồ uống</span>
-              <div class="category-menu-item_right">
-                <span class="material-icons-outlined">chevron_right</span>
-              </div>
-            </a>
-          </li>
-          <li class="category-menu-item">
-            <a class="category-menu-item__url" href="/giai-tri">
-              <div class="category-menu-item__url__left">
-                <span class="material-icons-outlined">theaters</span>
-              </div>
-              <span>Giải trí</span>
-              <div class="category-menu-item_right">
-                <span class="material-icons-outlined">chevron_right</span>
-              </div>
-            </a>
-          </li>
-          <li class="category-menu-item">
-            <a class="category-menu-item__url" href="/lam-dep">
-              <div class="category-menu-item__url__left">
-                <span class="material-icons-outlined">heart_broken</span>
-              </div>
-              <span>Làm đẹp</span>
-              <div class="category-menu-item_right">
-                <span class="material-icons-outlined">chevron_right</span>
-              </div>
-            </a>
-          </li>
-          <li class="category-menu-item">
-            <a class="category-menu-item__url" href="/khac">
-              <div class="category-menu-item__url__left">
-                <span class="material-icons">more_horiz</span>
-              </div><span>Khác</span>
-              <div class="category-menu-item_right">
-                <span class="material-icons-outlined">chevron_right</span>
-              </div>
-            </a>
-          </li>
-        </ul>
+    <main class="container">
+        <div class="home_page_1">
+            <div class="col-md-4 mt-4 mx-5" style="width: 279px;">
+                <div class="list-group">
+                    <a href="#" class="list-group-item list-group-item-action bg-danger text-white " style="height: 51px;" aria-current="true">Đặt chỗ khuyến mãi</a>
 
-        <ul class="home-page-category-list">
-          <li class="category-menu-item category-menu-item--header">
-            <a href="javascript:;" class="category-menu-item__url">
-              <span>Khám phá</span>
-            </a>
-          </li>
-          <li class="category-menu-item">
-            <a class="category-menu-item__url" href="/an">
-              <div class="category-menu-item__url__left">
-                <span class="material-icons-outlined">military_tech</span>
-              </div>
-              <span>Độc quyền - Chỉ có tại JAMJA</span>
-              <div class="category-menu-item_right">
-                <span class="material-icons-outlined">chevron_right</span>
-              </div>
-            </a>
-          </li>
-          <li class="category-menu-item">
-            <a class="category-menu-item__url" href="/uong">
-              <div class="category-menu-item__url__left">
-                <span class="material-icons-outlined">collections_bookmark</span>
-              </div>
-              <span>Bộ sưu tập</span>
-              <div class="category-menu-item_right">
-                <span class="material-icons-outlined">chevron_right</span>
-              </div>
-            </a>
-          </li>
-          <li class="category-menu-item">
-            <a class="category-menu-item__url" href="/giai-tri">
-              <div class="category-menu-item__url__left">
-                <span class="material-icons-outlined">store_mall_directory</span>
-              </div>
-              <span>Trung tâm thương mại</span>
-              <div class="category-menu-item_right">
-                <span class="material-icons-outlined">chevron_right</span>
-              </div>
-            </a>
-          </li>
-        </ul>
-      </div>  
+                </div>
+                <div class="list-group mt-3">
+                    <a href="#" class="list-group-item list-group-item-action" aria-current="true">Danh mục</a>
+                    <a href="#" class="list-group-item list-group-item-action ">Đồ ăn</a>
+                    <a href="#" class="list-group-item list-group-item-action">Đồ uống</a>
+                    <a href="#" class="list-group-item list-group-item-action">Giải trí</a>
+                    <a href="#" class="list-group-item list-group-item-action ">Làm đẹp</a>
+                    <a href="#" class="list-group-item list-group-item-action ">Khác</a>
+                </div>
+                <div class="list-group mt-3">
+                    <a href="#" class="list-group-item list-group-item-action" aria-current="true">Khám phá</a>
+                    <a href="#" class="list-group-item list-group-item-action ">Độc quyền - Chỉ có tại JAMJA</a>
+                    <a href="#" class="list-group-item list-group-item-action">Bộ sưu tập</a>
+                    <a href="#" class="list-group-item list-group-item-action">Trung tâm thương mại</a>
+                   
+                </div>
+
+            </div>
+        </div>
+        <div class="home_page_2 mx-5">
+            <div class="col-md">
+                <img src="img/jamja-xmas.png" class="img-fluid mt-5" alt="...">
+            </div>
+        </div>
+        <div class="home_page_3 mx-5">
+            <div class="col-md-3">
+                <h2 class="should-see text-secondary">DÀNH CHO NGƯỜI DÙNG MỚI</h2>
+                    <div class="border">
+
+                    </div>
+            </div>
+            <div class="col-md-3">
+            <div class="card" style="width: 18rem;">
+                <img src="img/sharetea.png" class="card-img-top" alt="...">
+                <img src="img/danhchonguoidungmoi.jpg" class="card-img" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">An item</li>
+                    <li class="list-group-item">A second item</li>
+                    <li class="list-group-item">A third item</li>
+                </ul>
+                <div class="card-body">
+                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a>
+                </div>
+                </div>
+            </div>
+        </div>
+        <div class="home_page_4">
+            
+        </div>
+    </main>
 
 
 
 
-      
-    </div>
-</div>
-
-
-  
-    
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-        crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+        </script>
 </body>
+
 </html>
