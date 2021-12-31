d7c1d0e3e560e38026bcd9da32cea527b8d14bf7
49_1951060914_NguyenHongPhong <nguyenphong15022001@gmail.com>
2021-12-31 07:07:09 +0700

    upload file

--- giamgia.php
+++ giamgia.php
@@ -194,93 +194,65 @@
                             </ul>
                         </div>
                     </div>
-                    <div class="home-page__block__right">
-                        <div>
-                            <div class="top-feature-heading-homepage">
-                                <h2></h2>
-                                <div class="border">
 
-                                </div>
-                            </div>
-                            <div class="home-page__block__right_top_feature">
-                                <div class="top-feature-col-left">
-                                    <div class="top-feature-deal-item">
-                                        <!-- react-empty: 422 -->
-                                    </div>
-                                </div>
-                                <div class="top-feature-col-right">
-                                    <div class="top-feature-deal-item">
-                                        <!-- react-empty: 425 -->
-                                    </div>
-                                    <div class="top-feature-deal-item">
-                                        <!-- react-empty: 427 -->
-                                    </div>
-                                </div>
-                            </div>
+                </div>
+                <div class="category-block list-cell">
+                    <div class="category-header">
+                        <div class="heading">
+                            <h2>DÀNH CHO NGƯỜI DÙNG MỚI</h2>
+                          
                         </div>
-                        <div>
-                            <div class="top-feature-heading-homepage">
-                                <h2 class="should-see">CÓ THỂ BẠN NÊN XEM</h2>
-                                <div class="border"></div>
-                            </div>
-                            <div class="top-deal-suggest-container">
-                                <div class="top-deal-suggest">
-                                    <div class="deal-wrapper">
-                                        <a class="feature-item"
-                                            href="/khuyen-mai/luxspa/luxspachi-99k-tri-mun-don-tet-cho-da-dau-mun-an-mun-viem-mun-dau-den-0?internal=home-pin">
-                                            <img class="feature-item__thumb" alt="JAMJA tiết kiệm hơn"
-                                                src="https://img.jamja.vn/jamja-prod/image-4-268408.png?w=556">
-                                            <img src="/assets/images/img-gradient.png" alt="JAMJA tiết kiệm hơn"
-                                                class="gradient">
-                                            <div class="deal-item-content">
-                                                <div class="feature-item__content__wrapper">
-                                                    <div class="btn feature-item__cta">Đặt chỗ</div>
-                                                    <div class="text-center">
-                                                        <div class="feature-item__brand-name">LuxSpa - Chuyên điều trị
-                                                            Mụn Thâm Sẹo #1</div>
-                                                        <div class="feature-item__highlight">-53%</div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </a>
-                                    </div>
-                                    <div class="deal-wrapper"><a class="feature-item"
-                                            href="/khuyen-mai/ich-hoa-duong/ich-hoa-duong-chi-tu-18k-toan-bo-menu-do-uong-0?internal=home-pin">
-                                            <img class="feature-item__thumb" alt="JAMJA tiết kiệm hơn"
-                                                src="https://img.jamja.vn/jamja-prod/d9e6fb07-b0f1-49ed-bd61-ff8729f25028-68709.png?w=556">
-                                            <img src="/assets/images/img-gradient.png" alt="JAMJA tiết kiệm hơn"
-                                                class="gradient">
-                                            <div class="deal-item-content">
-                                                <div class="feature-item__content__wrapper">
-                                                    <div class="btn feature-item__cta">Đặt chỗ</div>
-                                                    <div class="text-center">
-                                                        <div class="feature-item__brand-name">Ích Hòa Đường </div>
-                                                        <div class="feature-item__highlight">-30%</div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </a>
-                                    </div>
-                                    <div class="deal-wrapper">
-                                        <a class="feature-item"
-                                            href="/khuyen-mai/kimho-bbq-buffet-nuong-lau/kimho-bbq-buffet-nuong-lau-chi-tu-139k-buffet-nuong-lau-0?internal=home-pin">
-                                            <img class="feature-item__thumb" alt="JAMJA tiết kiệm hơn"
-                                                src="https://img.jamja.vn/jamja-prod/86281672-707130166491622-9175386415955968000-o.jpg?w=556">
-                                            <img src="/assets/images/img-gradient.png" alt="JAMJA tiết kiệm hơn"
-                                                class="gradient">
-                                            <div class="deal-item-content">
-                                                <div class="feature-item__content__wrapper">
-                                                    <div class="btn feature-item__cta">Đặt chỗ</div>
-                                                    <div class="text-center">
-                                                        <div class="feature-item__brand-name">KIMHO BBQ - Buffet Nướng
-                                                            &amp; Lẩu</div>
-                                                        <div class="feature-item__highlight">-13%*</div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </a>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4 deal-item-wrapper ">
+                            <div class="deal-item">
+                                <div class="deal-item__header"><a class="deal-item__header__brand" target="_blank"
+                                        title="Sharetea" href="/thuong-hieu/sharetea?internal=new-user"><img
+                                            class="deal-item__header__brand__logo"
+                                            src="https://img.jamja.vn/jamja-prod/gcs_full_5971e23876ec57436b335f74-2017-07-21-111505.png?h=40"
+                                            alt="Sharetea" title="Sharetea"></a>
+                                    <div class="deal-item__header__follow"><span class="material-icons">add</span></div>
+                                </div>
+                                <div class="deal-item__body"><a class="deal-item__body__thumbnail__wrapper"
+                                        href="/khuyen-mai/sharetea/sharetea-chi-1d-tra-xanh-sua-0?internal=new-user"><img
+                                            class="deal-item__body__thumbnail" title="Sharetea"
+                                            src="https://img.jamja.vn/jamja-prod/b102357b70408d1ed451.jpg?w=540">
+                                        <div class="deal-item__highlight__tag__wrapper">
+                                            <div class="deal-item__tag">1Đ</div>
+                                        </div>
+                                        <div class="deal-item__badge exclusive"><span class="material-icons-outlined">military_tech</span>
+                                        </div>
+                                        
+                                    </a></div>
+                                <h3 class="deal-item__footer__heading"><a
+                                        href="/khuyen-mai/sharetea/sharetea-chi-1d-tra-xanh-sua-0?internal=new-user">
+                                        <div class="deal-item__footer-badge exclusive">
+                                            <span class="material-icons">credit_score</span><!-- react-text: 364 -->Đặt chỗ
+                                            <!-- /react-text -->
+                                        </div><!-- react-text: 365 -->Sharetea: CHỈ 1Đ TRÀ XANH SỮA
+                                        <!-- /react-text -->
+                                    </a></h3>
+                                <div class="deal-item__meta-text">
+                                    <div class="bookmark"><span class="jicon_bookmark_o"></span><span>110</span></div>
+                                    <div class="rating small deal-item__rating" title="Điểm: 4.65"><span
+                                            class="jicon_star_full" style="color: rgb(239, 134, 59);"></span><span
+                                            class="jicon_star_full" style="color: rgb(239, 134, 59);"></span><span
+                                            class="jicon_star_full" style="color: rgb(239, 134, 59);"></span><span
+                                            class="jicon_star_full" style="color: rgb(239, 134, 59);"></span><span
+                                            class="jicon_star_haft_o" style="color: rgb(239, 134, 59);"></span>
+                                        <!-- react-text: 376 -->
+                                        <!-- /react-text -->
+                                        <!-- react-text: 377 -->81
+                                        <!-- /react-text -->
+                                        <!-- react-text: 378 --> •
+                                        <!-- /react-text -->
+                                        <!-- react-text: 379 -->946
+                                        <!-- /react-text -->
+                                        <!-- react-text: 380 --> lượt đặt
+                                        <!-- /react-text -->
                                     </div>
                                 </div>
+                                <div class="deal-item__address-text"> 104 Cầu Gỗ, Hàng Trống, Hoàn Kiếm, Hà Nội</div>
                             </div>
                         </div>
                     </div>
@@ -302,10 +274,10 @@
                     <h3 class="tai-mien-phi">Tải MIỄN PHÍ</h3>
                     <div class="download-app">
                         <a href="https://itunes.apple.com/us/app/jamja-giam-gia-coupon-deal/id980165889?mt=8"
-                            class="ios" rel="nofollow"><img src="img/app-store.svg"alt="">
+                            class="ios" rel="nofollow"><img src="img/app-store.svg" alt="">
                         </a>
-                        <a href="https://play.google.com/store/apps/details?id=com.jamjavn.jamja"
-                            class="android" rel="nofollow"><img src="img/gg-play.svg" alt="">
+                        <a href="https://play.google.com/store/apps/details?id=com.jamjavn.jamja" class="android"
+                            rel="nofollow"><img src="img/gg-play.svg" alt="">
                         </a>
                     </div>
                 </div>
@@ -314,8 +286,8 @@
                     <form id="mc-form">
                         <div class="input-group input-group-sx"><input type="email" class="form-control email-input "
                                 placeholder="Địa chỉ email của bạn">
-                            
-                            
+
+
                             <span class="input-group-btn">
                                 <button id="email-sub" class="btn btn-default email-button" type="button">
                                     <span class="material-icons">chevron_right</span>
@@ -385,14 +357,12 @@
                                 data-small-header="true" fb-xfbml-state="rendered"
                                 fb-iframe-plugin-query="adapt_container_width=true&amp;app_id=437382966424613&amp;container_width=445&amp;height=154&amp;hide_cover=false&amp;hide_cta=false&amp;href=https%3A%2F%2Fwww.facebook.com%2Fjamja.vn%2F&amp;locale=en_US&amp;sdk=joey&amp;show_facepile=true&amp;small_header=true&amp;tabs=&amp;width=458">
                                 <span style="vertical-align: bottom; width: 445px; height: 70px;">
-                                    <iframe
-                                        name="f9ca27ad303028" width="458px" height="154px"
+                                    <iframe name="f9ca27ad303028" width="458px" height="154px"
                                         data-testid="fb:page Facebook Social Plugin"
                                         title="fb:page Facebook Social Plugin" frameborder="0" allowtransparency="true"
                                         allowfullscreen="true" scrolling="no" allow="encrypted-media"
                                         src="https://www.facebook.com/v2.8/plugins/page.php?adapt_container_width=true&amp;app_id=437382966424613&amp;channel=https%3A%2F%2Fstaticxx.facebook.com%2Fx%2Fconnect%2Fxd_arbiter%2F%3Fversion%3D46%23cb%3Dfe41f82f5e845c%26domain%3Djamja.vn%26is_canvas%3Dfalse%26origin%3Dhttps%253A%252F%252Fjamja.vn%252Ffd5e052f9aeadc%26relation%3Dparent.parent&amp;container_width=445&amp;height=154&amp;hide_cover=false&amp;hide_cta=false&amp;href=https%3A%2F%2Fwww.facebook.com%2Fjamja.vn%2F&amp;locale=en_US&amp;sdk=joey&amp;show_facepile=true&amp;small_header=true&amp;tabs=&amp;width=458"
-                                        style="border: none; visibility: visible; width: 445px; height: 70px;"
-                                        class="">
+                                        style="border: none; visibility: visible; width: 445px; height: 70px;" class="">
                                     </iframe>
                                 </span>
                             </div>
@@ -409,15 +379,18 @@
                         <span>©2019 JAMJA Corp.</span><span>Công ty cổ phần JAMJA - Địa chỉ: Tầng 5, số 71 Nguyễn Chí
                             Thanh, Phường Láng Hạ, Quận Đống Đa, Hà Nội - Mã số thuế: 0107424266</span><span>GCNĐKDN cấp
                             lần đầu ngày 06/05/2016, thay đổi lần thứ 5 ngày 02/10/2019 - Nơi cấp: Sở KH&amp;ĐT thành
-                            phố Hà Nội - Người đại diện: Ông Lê Hùng Việt</span></div>
-                    <div style="flex-basis: 250px;"><a
-                            href="http://online.gov.vn/HomePage/WebsiteDisplay.aspx?DocId=29352" target="_blank"
-                            rel="nofollow"><img class="icon-bo-cong-thuong" alt="" title=""
-                                src="/assets/images/bocongthuong.jpg"></a><a target="_blank" rel="nofollow"
+                            phố Hà Nội - Người đại diện: Ông Lê Hùng Việt</span>
+                    </div>
+                    <div class="st" style="flex-basis: 250px;">
+                        <a href="http://online.gov.vn/HomePage/WebsiteDisplay.aspx?DocId=29352" target="_blank"
+                            rel="nofollow"><img class="icon-bo-cong-thuong" alt="" title="" src="img/bocongthuong.jpg">
+                        </a>
+                        <a target="_blank" rel="nofollow"
                             href="//www.dmca.com/Protection/Status.aspx?ID=ccbb45fd-c6a5-4642-a2bf-1e3d9a383c97"
                             title="DMCA.com Protection Status" class="dmca-badge"><img class="icon-dcma"
                                 src="//images.dmca.com/Badges/dmca-badge-w250-5x1-02.png?ID=ccbb45fd-c6a5-4642-a2bf-1e3d9a383c97"
-                                alt="DMCA.com Protection Status"></a>
+                                alt="DMCA.com Protection Status">
+                        </a>
                         <script src="//images.dmca.com/Badges/DMCABadgeHelper.min.js"> </script>
                     </div>
                 </div>
