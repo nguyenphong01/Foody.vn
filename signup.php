f6c6bf5195e1bfac857f400d7e4c307157b9efcf
49_1951060914_NguyenHongPhong <nguyenphong15022001@gmail.com>
2022-01-17 21:08:54 +0700

    Update signup.php

--- signup.php
+++ signup.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create a Meetup Account | Meetup</title>
+    <title>Sign up</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
@@ -33,7 +33,7 @@
                                             Đăng kí
                                         </h5>
                                              <a class="text-decoration-none"
-                                                href="">Đăng nhập</a>
+                                                href="login.php">Đăng nhập</a>
                                         </div>
                                         
                                         <label for="inputEmail" class="sr-only">Email
